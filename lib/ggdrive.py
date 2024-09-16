228facc553ea1358981c49a6a159e81d31e63458
levuloihust99 <49064246+levuloihust99@users.noreply.github.com>
2024-09-16 10:41:16 +0700

    add --recursive option

--- lib/ggdrive.py
+++ lib/ggdrive.py
@@ -4,6 +4,7 @@ import io
 import json
 import httpx
 import argparse
+from typing import Optional, Union
 
 from urllib.parse import urljoin
 from urllib3.filepost import choose_boundary
@@ -24,7 +25,11 @@ SCOPES = [
     "https://www.googleapis.com/auth/drive.metadata",
 ]
 DRIVE_API_BASE_URL = "https://www.googleapis.com/upload/drive/v3/"
+DRIVE_API_FILE_ENDPOINT = urljoin(DRIVE_API_BASE_URL, "files")
 FILE_SIZE_THRESHOLD = 5 * 1024**2
+CREDS = {"value": None}
+SERVICE = {"value": None}
+
 client_secrets_file = "client_secrets.json"
 token_file = "token.json"
 
@@ -56,11 +61,169 @@ def authenticate():
     return creds
 
 
-def upload(file_path: str, parent_id: str):
-    creds = authenticate()
-    authorization_headers = {"Authorization": "Bearer {}".format(creds.token)}
-    service = build("drive", "v3", credentials=creds)
+def get_creds():
+    if not CREDS["value"]:
+        CREDS["value"] = authenticate()
+    creds = CREDS["value"]
+    return creds
 
+
+def get_service():
+    if not SERVICE["value"]:
+        creds = get_creds()
+        SERVICE["value"] = build("drive", "v3", credentials=creds)
+    return SERVICE["value"]
+
+
+def make_request(
+    url: str,
+    method: str,
+    client: Optional[httpx.Client] = None,
+    params: Optional[dict] = None,
+    content: Optional[bytes] = None,
+    json_data: Optional[Union[list, dict]] = None,
+    headers: Optional[dict] = None,
+):
+    kwargs = {}
+    if params is not None:
+        kwargs.update(params=params)
+    if content is not None:
+        kwargs.update(content=content)
+    if json_data is not None:
+        kwargs.update(json=json_data)
+    if headers is not None:
+        kwargs.update(headers=headers)
+    if client:
+        resp = client.request(method=method, url=url, **kwargs)
+    else:
+        with httpx.Client(timeout=60) as client:
+            resp = client.request(method=method, url=url, **kwargs)
+    return resp
+
+
+def upload_single(
+    file_path: str,
+    parent_id: str,
+    client: Optional[httpx.Client] = None,
+    id_tracker: Optional[dict] = None,
+    iterating_status_tracker: Optional[str] = None,
+    prefix: Optional[str] = "File",
+):
+    creds = get_creds()
+    authorization_headers = {"Authorization": "Bearer {}".format(creds.token)}
+    service = get_service()
+
+    if isinstance(iterating_status_tracker, dict):
+        iterating_status = iterating_status_tracker.get("value", "")
+    else:
+        iterating_status = ""
+    flush_string = "\r" + " " * len(iterating_status) + "\r"
+    iterating_status = "{}: {}".format(
+        prefix, format_print_path(file_path, max_line_len=150)
+    )
+    print(flush_string + iterating_status, end="")
+    if isinstance(iterating_status_tracker, dict):
+        iterating_status_tracker["value"] = iterating_status
+
+    if os.path.isfile(file_path):
+        file_size = os.path.getsize(file_path)
+        with open(file_path, "rb") as reader:
+            file_content = reader.read()
+        file_metadata = {"name": os.path.basename(file_path), "parents": [parent_id]}
+        if file_size < FILE_SIZE_THRESHOLD:  # multipart upload
+            boundary = choose_boundary()
+            encoded_boundary = boundary.encode()
+            headers = {
+                "Content-Type": 'multipart/related; boundary="{}"'.format(boundary)
+            }
+            body = b""
+            body += b"--" + encoded_boundary + b"\n"
+            body += b"Content-Type: application/json; charset=UTF-8\n\n"
+            body += json.dumps(file_metadata).encode() + b"\n"
+            body += b"--" + encoded_boundary + b"\n"
+            body += b"Content-Type: application/octet-stream\n\n"
+            body += file_content + b"\n"
+            body += b"--" + encoded_boundary + b"--"
+            resp = make_request(
+                url=DRIVE_API_FILE_ENDPOINT,
+                method="post",
+                client=client,
+                content=body,
+                headers={**headers, **authorization_headers},
+            )
+            check_for_error(resp)
+        else:  # use resumable upload
+            resp = client.post(
+                DRIVE_API_FILE_ENDPOINT,
+                params={"uploadType": "resumable"},
+                json=file_metadata,
+                headers={
+                    "Content-Type": "application/json; charset=UTF-8",
+                    **authorization_headers,
+                },
+            )
+            check_for_error(resp)
+            session_uri = resp.headers.get("location")
+            bytes_sent = 0
+            chunk_size = FILE_SIZE_THRESHOLD
+            while True:
+                chunk = file_content[bytes_sent : bytes_sent + chunk_size]
+                if len(chunk) == 0:
+                    break
+                resp = make_request(
+                    url=session_uri,
+                    method="put",
+                    client=client,
+                    content=chunk,
+                    headers={
+                        **{
+                            "Content-Length": str(len(chunk)),
+                            "Content-Range": "bytes {}-{}/{}".format(
+                                bytes_sent,
+                                bytes_sent + len(chunk) - 1,
+                                file_size,
+                            ),
+                        },
+                        **authorization_headers,
+                    },
+                )
+                if resp.status_code < 300:
+                    break
+                elif resp.status_code == 308:
+                    sent_range = resp.headers.get("range", "")
+                    match = re.match(r"^bytes=\d+-(?P<last_byte>\d+)$", sent_range)
+                    if not match:
+                        raise Exception("Cannot determine bytes range")
+                    bytes_sent = int(match.group("last_byte")) + 1
+                    if isinstance(iterating_status_tracker, dict):
+                        iterating_status = iterating_status_tracker.get("value", "")
+                    else:
+                        iterating_status = ""
+                    flush_string = "\r" + " " * len(iterating_status) + "\r"
+                    iterating_status = "{} ({}): {}".format(
+                        prefix,
+                        "{}/{}".format(bytes_sent, file_size),
+                        format_print_path(file_path),
+                    )
+                    if isinstance(iterating_status_tracker, dict):
+                        iterating_status_tracker["value"] = iterating_status
+                    print(flush_string + iterating_status, end="")
+                else:
+                    raise Exception(resp.content.decode())
+    else:
+        file_metadata = {
+            "name": os.path.basename(file_path),
+            "parents": [parent_id],
+            "mimeType": "application/vnd.google-apps.folder",
+        }
+        uploaded_folder = (
+            service.files().create(body=file_metadata, fields="id").execute()
+        )
+        if isinstance(id_tracker, dict):
+            id_tracker[file_path] = uploaded_folder.get("id")
+
+
+def upload(file_path: str, parent_id: str):
     print("Scanning...")
     sequence = list_files(
         file_path,
@@ -74,108 +237,21 @@ def upload(file_path: str, parent_id: str):
     count = len(sequence)
     max_num_width = len(str(count))
     counter_prefix_template = "#File {:0%d}/{}" % max_num_width
-    drive_api_file_endpoint = urljoin(DRIVE_API_BASE_URL, "files")
-    iterating_status = ""
+    iterating_status_tracker = {}
 
     with httpx.Client(timeout=60) as client:
         for i, f in enumerate(sequence):
-            counter_prefix = counter_prefix_template.format(i + 1, count)
-            flush_string = "\r" + " " * len(iterating_status) + "\r"
-            iterating_status = "{}: {}".format(
-                counter_prefix, format_print_path(f, max_line_len=150)
-            )
-            print(flush_string + iterating_status, end="")
+            prefix = counter_prefix_template.format(i + 1, count)
             f_dir = os.path.dirname(f)
-            _parent = id_tracker.get(f_dir, parent_id)
-            if os.path.isfile(f):
-                file_size = os.path.getsize(f)
-                with open(f, "rb") as reader:
-                    file_content = reader.read()
-                file_metadata = {"name": os.path.basename(f), "parents": [_parent]}
-                if file_size < FILE_SIZE_THRESHOLD:  # multipart upload
-                    boundary = choose_boundary()
-                    encoded_boundary = boundary.encode()
-                    headers = {
-                        "Content-Type": 'multipart/related; boundary="{}"'.format(
-                            boundary
-                        )
-                    }
-                    body = b""
-                    body += b"--" + encoded_boundary + b"\n"
-                    body += b"Content-Type: application/json; charset=UTF-8\n\n"
-                    body += json.dumps(file_metadata).encode() + b"\n"
-                    body += b"--" + encoded_boundary + b"\n"
-                    body += b"Content-Type: application/octet-stream\n\n"
-                    body += file_content + b"\n"
-                    body += b"--" + encoded_boundary + b"--"
-                    resp = client.post(
-                        drive_api_file_endpoint,
-                        content=body,
-                        headers={**headers, **authorization_headers},
-                    )
-                    check_for_error(resp)
-                else:  # use resumable upload
-                    resp = client.post(
-                        drive_api_file_endpoint,
-                        params={"uploadType": "resumable"},
-                        json=file_metadata,
-                        headers={
-                            "Content-Type": "application/json; charset=UTF-8",
-                            **authorization_headers,
-                        },
-                    )
-                    check_for_error(resp)
-                    session_uri = resp.headers.get("location")
-                    bytes_sent = 0
-                    chunk_size = FILE_SIZE_THRESHOLD
-                    while True:
-                        chunk = file_content[bytes_sent : bytes_sent + chunk_size]
-                        if len(chunk) == 0:
-                            break
-                        resp = client.put(
-                            session_uri,
-                            content=chunk,
-                            headers={
-                                **{
-                                    "Content-Length": str(len(chunk)),
-                                    "Content-Range": "bytes {}-{}/{}".format(
-                                        bytes_sent,
-                                        bytes_sent + len(chunk) - 1,
-                                        file_size,
-                                    ),
-                                },
-                                **authorization_headers,
-                            },
-                        )
-                        if resp.status_code < 300:
-                            break
-                        elif resp.status_code == 308:
-                            sent_range = resp.headers.get("range", "")
-                            match = re.match(
-                                r"^bytes=\d+-(?P<last_byte>\d+)$", sent_range
-                            )
-                            if not match:
-                                raise Exception("Cannot determine bytes range")
-                            bytes_sent = int(match.group("last_byte")) + 1
-                            flush_string = "\r" + " " * len(iterating_status) + "\r"
-                            iterating_status = "{} ({}): {}".format(
-                                counter_prefix,
-                                "{}/{}".format(bytes_sent, file_size),
-                                format_print_path(f),
-                            )
-                            print(flush_string + iterating_status, end="")
-                        else:
-                            raise Exception(resp.content.decode())
-            else:
-                file_metadata = {
-                    "name": os.path.basename(f),
-                    "parents": [_parent],
-                    "mimeType": "application/vnd.google-apps.folder",
-                }
-                uploaded_folder = (
-                    service.files().create(body=file_metadata, fields="id").execute()
-                )
-                id_tracker[f] = uploaded_folder.get("id")
+            parent_id = id_tracker.get(f_dir, parent_id)
+            upload_single(
+                file_path=f,
+                parent_id=parent_id,
+                client=client,
+                id_tracker=id_tracker,
+                iterating_status_tracker=iterating_status_tracker,
+                prefix=prefix,
+            )
 
         # end loop, print newline
         print()
@@ -302,6 +378,12 @@ def main():
         help="If True, files not matching `ignore_pattern` or matching `include_pattern` will be included. "
         "Else, files matching `include_pattern` and not matching `ignore_pattern` will be included.",
     )
+    parser.add_argument(
+        "--recursive",
+        default=True,
+        type=eval,
+        help="Whether to upload the folder recursively."
+    )
     args = parser.parse_args()
 
     global include_pattern, ignore_pattern, include_over_ignore, client_secrets_file, token_file
@@ -311,7 +393,11 @@ def main():
     include_over_ignore = args.include_over_ignore
 
     if args.command == "up":
-        upload(args.file_path, args.parent_id)
+        if args.recursive:
+            upload(args.file_path, args.parent_id)
+        else:
+            upload_single(args.file_path, args.parent_id)
+            print()
     else:
         download(args.file_id, args.download_dir or os.getcwd())
 
